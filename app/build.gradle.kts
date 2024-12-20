f5f1d80a47573c13a811c5541e6482c5a43dfbd9
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2024-12-20 18:47:30 +0300

    Update for 2 step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
@@ -13,6 +16,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
 
 tasks.test {
     useJUnitPlatform()
