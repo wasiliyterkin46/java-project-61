6f4a9b6fc000c0282c144fd15e40b12da9d916e1
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2024-12-22 15:30:26 +0300

    Update hexlet/code/App.java && hexlet/code/Cli.java

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void helloAny() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+
+        System.out.println("Hello, " + name + "!");
+        in.close();
+    }
+}
